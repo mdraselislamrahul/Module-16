ec2f1dffd3578543b6dbba6b7cddac1f791bb4d5
mdraselislamrahul <mdraselislamrahul@gmail.com>
2023-08-14 21:51:36 +0600

    Second commit

--- Video-3.js
+++ Video-3.js
@@ -1,14 +1,7 @@
-// numberic
+// Number type variable
 var price = 11;
-var age = 20;
-var tamparacher = 30;
-var width = 54;
-var mony = 300;
-var mamberoffamily = 7;
-// string
-var persion = "Md Rasel Islam";
-var location = "Panchagarh Debigonj Bhawlagonj";
-var spacial = 'jone sings';
-// boolean
-var serius = true;
-var single = false;
+var age = 19;
+var trampracher = 38;
+var width = 55;
+var mony = 30;
+var people = 7;
---
+++ Video-4.js
@@ -0,0 +1,9 @@
+// string type var
+var person = "MD. Rasel islam";
+var location = "andor killah bandor bon";
+var spasial = 'Aliya vat';
+
+// Boolean Type
+var serious = true;
+var isFullMark = true;
+var isSingle = true;
