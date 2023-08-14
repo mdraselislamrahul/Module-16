ad80ba6d0c677b4f4b9012af31b8b8dd70eb4a01
mdraselislamrahul <mdraselislamrahul@gmail.com>
2023-08-14 22:06:22 +0600

    3ed commit

---
+++ Video-5.js
@@ -0,0 +1,10 @@
+// var name can not be any Keywords
+var false = 99;
+var
+return = true;
+
+// var name has to be in one work, no space
+var my home address = "new calyforniys";
+
+// var name can not strats with a number
+var 99 clup = 1964;
